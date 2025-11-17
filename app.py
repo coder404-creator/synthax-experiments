ab9e1d195d623294dca6e35fd289ba18adcc853f
Shreyas Ranganatha <shreyass.ranganatha@gmail.com>
2025-11-17 20:25:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 
-for i in range(100):
-    print('*' * i)
+choice = input("do you want to run this command? ").lower()
+
+if choice == 'yes':
+    for i in range(10):
+        print('*' * i)
+else:
+    print("CLOSING")
